334ccbd3cddca54adaa15fe1644897a60d6089e0
Supaicy <143978573+supaicy@users.noreply.github.com>
2024-03-27 10:47:17 +0900

    init

---
+++ src/main/java/live/smoothing/eureka/SmoothingEurekaApplication.java
@@ -0,0 +1,14 @@
+package live.smoothing.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothingEurekaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SmoothingEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/smoothing/eureka/SmoothingEurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package live.smoothing.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoothingEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
